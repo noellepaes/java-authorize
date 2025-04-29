9021eb054712356fda86f1d17d7a39bb8c7fb738
noellepaes <noellepaes12345@gmail.com>
2025-04-28 23:54:11 -0300

    feat: authorize

---
+++ src/main/java/com/noelle/authorize/dto/LoginRequestDTO.java
@@ -0,0 +1,5 @@
+package com.noelle.authorize.dto;
+
+public record LoginRequestDTO  (String email, String password){
+    
+}
---
+++ src/main/java/com/noelle/authorize/dto/ResponseDTO.java
@@ -0,0 +1,3 @@
+package com.noelle.authorize.dto;
+
+public record ResponseDTO (String name, String token) { }
